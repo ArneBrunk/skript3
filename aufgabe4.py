b080ff9e35b7870e45d88d2497abca4f1e0a679a
Arne Brunkert <arnebrunkert@gmail.com>
2021-04-13 12:16:03 +0200

    commit Aufgabe 4 erste versuche

---
+++ aufgabe4.py
@@ -0,0 +1,50 @@
+class Spiel():
+    def __init__ (self, shipname, titel):
+        shipCreator = EnemeyShipCreator()
+        self.ship = shipCreator.createShip(shipname, titel)
+
+
+#creator
+class EnemeyShipCreator():
+    def createShip(self, name, titel):
+        if name == "UFO":
+            ship = UFOEnemyShip(titel)
+        elif name == "Rocket":
+            ship = RocketEnemyShip(titel)
+        ship.__init__(titel)
+        return ship
+
+        
+
+#class Ship
+class Ship():
+    def __init__ (self, name):
+        self.name = name
+        self.damage = 50
+    def followHeroShip(self):
+        pass
+    def displayEnemyShip(self):
+        pass
+    def enemeyShipsShoots(self):
+        pass
+    def setDamage(self, dmg):
+        self.damage = dmg
+    def getDamage(self):
+        print(self.damage)
+
+#Schifftypen
+class RocketEnemyShip(Ship):
+    def setName(self, name):
+        self.name = name
+    
+    def getName(self):
+        print("Dies ist das Raketenschiff:", self.name)
+
+class UFOEnemyShip(Ship):
+    def setName(self, name):
+        self.name = name
+
+    def getName(self):
+        print("Dies ist das UFO:", self.name)
+
+newgame = Spiel("UFO", "Arne")
