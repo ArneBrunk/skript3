d2a2c9bd196e686a8c7ff6b74fc2b17214ee1695
Arne Brunkert <arnebrunkert@gmail.com>
2021-04-28 08:12:04 +0200

    Commit Aufgabe 2 mit Dict

---
+++ aufgabe2_1.py
@@ -0,0 +1,87 @@
+# Importiere Metaklasse
+import abc
+
+#Player
+class Player():
+    def __init__(self, player):
+        self.player = player  
+
+    def trinke(self):
+        print("Trinke")
+
+    def esse(self):
+        print("Esse")
+
+    def wandere(self):
+        print("Wandere")
+
+    def kaempfe(self):
+        print("Kämpfe")
+
+    def verteidige(self):
+        print("Verteidige")
+
+# Interface-Klasse-Command
+class Command (metaclass=abc.ABCMeta):
+    def __init__(self, player: Player):
+        self.player = player
+    
+    def execute(self):
+        raise NotImplementedError
+        
+class EsseKommando(Command):
+    def execute(self):
+        self.player.esse()
+
+class TrinkeKommando(Command):
+    def execute(self):
+        self.player.trinke()
+
+class VerteidigeKommando(Command):
+    def execute(self):
+        self.player.verteidige()
+
+class WandereKommando(Command):
+    def execute(self):
+        self.player.wandere()
+
+class KaempfeKommando(Command):
+    def execute(self):
+        self.player.kaempfe()
+
+class TrinkeKommando(Command):
+    def execute(self):
+        self.player.trinke()
+
+class KomboKommando(Command):
+    def execute(self):
+        self.player.trinke()
+        self.player.kaempfe()
+        self.player.wandere()
+
+
+class Invoker:
+    def __init__ (self):
+        self.taste = {}
+      
+    def belegeTaste(self, taste, command: Command):
+        self.taste[taste] = command
+
+    def drueckeTaste(self, taste):
+        self.taste[taste].execute()
+
+mario = Player("Mario")
+tastatur = Invoker()
+tastatur.belegeTaste("x", KaempfeKommando(mario))
+tastatur.belegeTaste("y", EsseKommando(mario))
+tastatur.belegeTaste("z", KomboKommando(mario))
+tastatur.drueckeTaste("x")
+tastatur.drueckeTaste("y")
+tastatur.drueckeTaste("z")
+
+
+
+
+
+
+
